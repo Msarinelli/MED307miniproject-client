ba94e63d7a0b27127c1a3c101b50b2eeb975ce6d
Hpouls <Hpouls20@student.aau.dk>
2021-11-07 17:24:55 +0100

    rettelse

--- MED307miniproject-client/src/Client/Client.java
+++ MED307miniproject-client/src/Client/Client.java
@@ -8,10 +8,7 @@ public class Client {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //Changed from
-        // new NewJFrame().setVisible(true);
-        // to
-        new javaapplication1.NewJFrame().setVisible(true);
+        new NewJFrame().setVisible(true);
     }
     
 }
